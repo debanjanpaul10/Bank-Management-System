b9adbff6d73913be251959a3c1529aecfbd0a37e
debanjanpaul10 <debanjanpaul10@gmail.com>
2020-06-22 20:47:17 +0530

    First Commit

---
+++ bankSystem.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class bankSystem {
+    public static void main(String[] args) {
+        System.out.println("Bank Admin Menu:");
+        System.out.println("Please Enter A Menu option:");
+        System.out.println("1. Add customer to the bank.");
+        System.out.println("2. Change customer name.");
+        System.out.println("3. Check account balance.");
+        System.out.println("4. Modify account balance.");
+        System.out.println("5. Summary of all accounts.");
+        System.out.println("0. Quit the system.");
+    }
+}
