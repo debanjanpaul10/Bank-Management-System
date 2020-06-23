dca4ffb10d913276b47b6981da706f7466b3d709
debanjanpaul10 <debanjanpaul10@gmail.com>
2020-06-23 20:44:04 +0530

    Add system clear screen and Change name, balance check and modify name

--- bankSystem.java
+++ bankSystem.java
@@ -15,12 +15,13 @@ public class bankSystem {
            
         int size = 0;
         double[] accBalance = new double[250];
-        String[] accNames = new string[250];
+        String[] accNames = new String[250];
             
         switch(choice) {
             case 1:
                 //  Add customer
                 {
+                    System.out.println('\f');
                     System.out.println("Add Customer Menu:");
                     System.out.println("Enter the account balance:");
                     double bal = in.nextDouble();
@@ -35,15 +36,43 @@ public class bankSystem {
                 break;
             case 2:
                 //  Change name
+                {
+                    System.out.println('\f');
+                    System.out.println("Change customer Name");
+                    System.out.println("Please enter customer ID to change their name: ");
+                    int id = in.nextInt();
+                    System.out.println("What is the customer\'s new name? ");
+                    in.hasNextLine();
+                    accNames[id] = in.nextLine();
+                }
                 break;
             case 3:
                 //  Check balance
+                {
+                    System.out.println('\f');
+                    System.out.println("Check balance menu");
+                    System.out.println("Please enter a customer ID to check balance: ");
+                    int id = in.nextInt();
+                    double balance = accBalance[id];
+                    System.out.println("The customer has ₹"+balance+" in their account");
+                }
                 break;
             case 4:
                 //  Modify
+                {
+                    System.out.println('\f');
+                    System.out.println("Modify the balance menu");
+                    System.out.println("Enter customer ID to edit the balance amount: ");
+                    int id = in.nextInt();
+                    System.out.println("Enter the new balance: ");
+                    accBalance[id] = in.nextDouble();
+                }
                 break;
             case 5:
                 //  Summary
+                {
+
+                }
                 break;
             case 0:
                 System.out.println("Thank you for using this application");
